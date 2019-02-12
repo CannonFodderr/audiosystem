008f236135f814146fb0fe4e900a63168ebd5741
Idan Izhaki <izhaki.idan@gmail.com>
2019-02-12 17:36:33 +0200

    Move some state variables to constructor on userContextStore class

--- src/contexts/userContext.js
+++ src/contexts/userContext.js
@@ -18,29 +18,15 @@ const peerConfig = {
 const INITIAL_STATE = {
     peer: null,
     isPeerInitialized: false,
-    call: null,
     isAdminConnected: false,
     isPlaying: false,
-    updateAdminTimer: null,
-    startCtxBtn: null,
-    userPlayer: null,
-    ctx: null,
-    sampleRate: null,
-    micGain: null,
-    playerGain: null,
     micStream: null,
-    micToAdminGain: null,
-    playerToAdminGain: null,
     playerStream: null,
     micStreamToOutput: null,
     clonedMicStream: null,
     clonedPlayerStream: null,
     micGainValue: 0.5,
     playerGainValue: 0.8,
-    analyser: null,
-    dataArray: null,
-    pitchArr: [],
-    detectPitch: pitchFinder.YIN(),
     isOscActive: true,
     isOscPlaying: false,
     oscGainValue: 0.001
@@ -48,37 +34,44 @@ const INITIAL_STATE = {
 
 export class UserContextStore extends Component{
     state = INITIAL_STATE
+    constructor(props){
+        super(props)
+        this.ctx = null;
+        this.micToAdminGain = null;
+        this.playerToAdminGain = null;
+        this.playerGain = null;
+        this.micGain = null;
+        this.userPlayer = null;
+        this.analyser = null;
+        this.sampleRate = null;
+        this.pitchArr = [];
+        this.detectPitch = pitchFinder.YIN();
+        this.updateAdminTimer = null;
+    }
     setPeerConnection = async () => {
         await this.setState({peer: new Peer(this.context.room.username, peerConfig ), isPeerInitialized: true });
     }
     initUserPlayback = async userPlayerElement => {
-        await this.setState({
-            ctx: new(window.AudioContext || window.webkitAudioContext)(),
-        })
-        await this.setState({
-            sampleRate: this.state.ctx.sampleRate,
-            analyser: this.state.ctx.createAnalyser(),
-            userPlayer: userPlayerElement
-        })
-        // SETUP ANALYSER
+        this.ctx = await new(window.AudioContext || window.webkitAudioContext)();
+        this.userPlayer = userPlayerElement;
+        this.analyser = this.ctx.createAnalyser();
+        this.sampleRate = this.ctx.sampleRate;
         // Capture Stream from audio player
         if(this.isChromeBrowser()){
-            this.setState({playerStream: this.state.userPlayer.captureStream()})
-            console.log("Chrome Capture");
+            this.setState({playerStream: this.userPlayer.captureStream()})
         } else {
-            this.setState({playerStream: this.state.userPlayer.mozCaptureStream()})
-            console.log("FireFox Capture");
+            this.setState({playerStream: this.userPlayer.mozCaptureStream()})
         }
-        await this.setState({playerGain: this.state.ctx.createGain()})
-        this.state.playerGain.gain.value = this.state.playerGainValue;
-        let playerStreamToOutput = this.state.ctx.createMediaStreamSource(this.state.playerStream);
-        let filter = this.state.ctx.createBiquadFilter();
+        this.playerGain = this.ctx.createGain()
+        this.playerGain.gain.value = this.state.playerGainValue;
+        let playerStreamToOutput = this.ctx.createMediaStreamSource(this.state.playerStream);
+        let filter = this.ctx.createBiquadFilter();
         filter.type = "highpass";
         filter.frequency.value = 250;
         playerStreamToOutput.connect(filter);
-        filter.connect(this.state.analyser);
-        this.state.analyser.connect(this.state.playerGain);
-        this.state.playerGain.connect(this.state.ctx.destination);
+        filter.connect(this.analyser);
+        this.analyser.connect(this.playerGain);
+        this.playerGain.connect(this.ctx.destination);
         this.getUserMicStream()
     }
     isChromeBrowser = () => {
@@ -94,14 +87,14 @@ export class UserContextStore extends Component{
         let constraints = { video: false, audio: true };
         navigator.mediaDevices.getUserMedia(constraints)
         .then(stream => {
+            this.micGain = this.ctx.createGain();
             this.setState({
                 micStream: stream, 
-                micGain: this.state.ctx.createGain()
             })
-            this.state.micGain.gain.value = this.state.micGainValue;
-            this.setState({micStreamToOutput: this.state.ctx.createMediaStreamSource(this.state.micStream)})
-            this.state.micStreamToOutput.connect(this.state.micGain);
-            this.state.micGain.connect(this.state.ctx.destination);
+            this.micGain.gain.value = this.state.micGainValue;
+            this.setState({micStreamToOutput: this.ctx.createMediaStreamSource(this.state.micStream)})
+            this.state.micStreamToOutput.connect(this.micGain);
+            this.micGain.connect(this.ctx.destination);
             console.log("Finished Mic Setup");
             this.setupConnection();
         })
@@ -121,8 +114,8 @@ export class UserContextStore extends Component{
         conn.on('close', () => {
             this.setState({isAdminConnected: false})
             console.log("Admin Disconnected")
-            clearInterval(this.state.updateAdminTimer);
-            this.setState({updateAdminTimer: null})
+            clearInterval(this.updateAdminTimer);
+            this.updateAdminTimer = null;
             // if(!this.state.isAdminConnected){
             //     reconnect();
             // }            
@@ -134,25 +127,25 @@ export class UserContextStore extends Component{
                     micGain: this.state.micGainValue, 
                     playerGain: this.state.playerGainValue, 
                     isPlaying: this.state.isPlaying, 
-                    playerTime: this.state.userPlayer.currentTime, 
+                    playerTime: this.userPlayer.currentTime, 
                     isAdminConnected: this.state.isAdminConnected 
                 });
             }
             if(data.cmd === "admin connect"){
                 console.log("Admin Connected");
-                this.setState({updateAdminTimer: setInterval(() => {
+                this.updateAdminTimer = setInterval(() => {
                     updateAdminUi()
-                }, 1000)})
+                }, 1000);
             }
             if(data.cmd === "mic gain"){
                 this.setState({micGainValue: data.value})
-                this.state.micGain.gain.setValueAtTime(data.value, this.state.ctx.currentTime);
-                this.state.micToAdminGain.gain.setValueAtTime(data.value, this.state.ctx.currentTime);
+                this.micGain.gain.setValueAtTime(data.value, this.ctx.currentTime);
+                this.micToAdminGain.gain.setValueAtTime(data.value, this.ctx.currentTime);
             }
             if(data.cmd === "player gain"){
                 this.setState({playerGainValue: data.value})
-                this.state.playerGain.gain.setValueAtTime(data.value, this.state.ctx.currentTime);
-                this.state.playerToAdminGain.gain.setValueAtTime(data.value, this.state.ctx.currentTime);
+                this.playerGain.gain.setValueAtTime(data.value, this.ctx.currentTime);
+                this.playerToAdminGain.gain.setValueAtTime(data.value, this.ctx.currentTime);
             }
             if(data.cmd === "osc state"){
                 if(data.value === false){
@@ -165,27 +158,27 @@ export class UserContextStore extends Component{
                 this.setState({oscGainValue: data.value})
             }
             if(data.cmd === "player start"){
-                this.state.userPlayer.play();
+                this.userPlayer.play();
                 this.setState({isPlaying: true})
                 this.pitchDetector();
             }
             if(data.cmd === "player pause"){
                 this.setState({isPlaying: false});
-                this.state.userPlayer.pause();
+                this.userPlayer.pause();
             }
             if(data.cmd === "player stop"){
                 this.setState({isPlaying: false})
-                this.state.userPlayer.currentTime = 0;
-                this.state.userPlayer.pause();
+                this.userPlayer.currentTime = 0;
+                this.userPlayer.pause();
             }
             if(data.cmd === "rewind"){
-                let newTransportPosition = this.state.userPlayer.currentTime - 30 > 0 ? this.state.userPlayer.currentTime - 30 : 0;
-                this.state.userPlayer.currentTime = newTransportPosition;
+                let newTransportPosition = this.userPlayer.currentTime - 30 > 0 ? this.userPlayer.currentTime - 30 : 0;
+                this.userPlayer.currentTime = newTransportPosition;
             }
             if(data.cmd === "fforward"){
-                let maxDuration = this.state.userPlayer.duration;
-                let newTransportPosition = this.state.userPlayer.currentTime + 30 < maxDuration ? this.state.userPlayer.currentTime + 30 : maxDuration;
-                this.state.userPlayer.currentTime = newTransportPosition;
+                let maxDuration = this.userPlayer.duration;
+                let newTransportPosition = this.userPlayer.currentTime + 30 < maxDuration ? this.userPlayer.currentTime + 30 : maxDuration;
+                this.userPlayer.currentTime = newTransportPosition;
             }
         });
         this.state.peer.on('call', (call) => {
@@ -196,18 +189,19 @@ export class UserContextStore extends Component{
     }
     createStreamToAdmin = async (call) => {
         await this.setState({clonedMicStream: this.state.micStream.clone(), clonedPlayerStream: this.state.playerStream.clone()})
-        let streamToAdmin = this.state.ctx.createMediaStreamDestination();
-        let micStreamToAdmin = this.state.ctx.createMediaStreamSource(this.state.clonedMicStream);
-        let playerStreamToAdmin = this.state.ctx.createMediaStreamSource(this.state.clonedPlayerStream);
+        let streamToAdmin = this.ctx.createMediaStreamDestination();
+        let micStreamToAdmin = this.ctx.createMediaStreamSource(this.state.clonedMicStream);
+        let playerStreamToAdmin = this.ctx.createMediaStreamSource(this.state.clonedPlayerStream);
         // Stream volume mix controls
-        await this.setState({micToAdminGain: this.state.ctx.createGain(), playerToAdminGain: this.state.ctx.createGain()})
+        this.micToAdminGain = this.ctx.createGain() 
+        this.playerToAdminGain = this.ctx.createGain()
         // Init mix controls gain
-        this.state.micToAdminGain.gain.setValueAtTime(this.state.micGainValue, this.state.ctx.currentTime);
-        this.state.playerToAdminGain.gain.setValueAtTime(this.state.playerGainValue, this.state.ctx.currentTime);
-        micStreamToAdmin.connect(this.state.micToAdminGain);
-        playerStreamToAdmin.connect(this.state.playerToAdminGain)
-        this.state.micToAdminGain.connect(streamToAdmin);
-        this.state.playerToAdminGain.connect(streamToAdmin);
+        this.micToAdminGain.gain.setValueAtTime(this.state.micGainValue, this.ctx.currentTime);
+        this.playerToAdminGain.gain.setValueAtTime(this.state.playerGainValue, this.ctx.currentTime);
+        micStreamToAdmin.connect(this.micToAdminGain);
+        playerStreamToAdmin.connect(this.playerToAdminGain)
+        this.micToAdminGain.connect(streamToAdmin);
+        this.playerToAdminGain.connect(streamToAdmin);
         let options = {
             'constraints': {
                 'mandatory': {
@@ -229,35 +223,34 @@ export class UserContextStore extends Component{
             return;
         }
         // Populate the dataArray from the analyser method
-        let dataArray = new Uint8Array(this.state.analyser.fftSize)
-        this.state.analyser.getByteTimeDomainData(dataArray);
+        let dataArray = new Uint8Array(this.analyser.fftSize)
+        this.analyser.getByteTimeDomainData(dataArray);
         // Detect pitch and push to array;
-        let pitch = this.state.detectPitch(dataArray, { sampleRate: 48000});
-        console.log(this.state.pitchArr, pitch);
-        this.setState({pitchArr: [...this.state.pitchArr, pitch]})
-        if(this.state.pitchArr.length > 2){
-            let newPitchArr = this.state.pitchArr;
+        let pitch = this.detectPitch(dataArray, { sampleRate: 48000});
+        this.pitchArr = [...this.pitchArr, pitch]
+        if(this.pitchArr.length > 2){
+            let newPitchArr = this.pitchArr;
             newPitchArr.shift()
-            this.setState({pitchArr: newPitchArr});
+            this.pitchArr = newPitchArr;
         }
         // Pervent overloading the oscillator
-        if(this.state.isOscActive && !this.state.isOscPlaying && this.state.pitchArr[1] && this.state.pitchArr[1] !== this.state.pitchArr[0]){
-            this.playOsc(this.state.pitchArr[1])
+        if(this.state.isOscActive && !this.state.isOscPlaying && this.pitchArr[1] && this.pitchArr[1] !== this.pitchArr[0]){
+            this.playOsc(this.pitchArr[1])
         }
         requestAnimationFrame(this.pitchDetector)
     }
     playOsc = pitch => {
-        let osc = this.state.ctx.createOscillator();
-        let oscGain = this.state.ctx.createGain();
+        let osc = this.ctx.createOscillator();
+        let oscGain = this.ctx.createGain();
         osc.type = 'sine';
-        osc.frequency.setValueAtTime(pitch*100, this.state.ctx.currentTime);
+        osc.frequency.setValueAtTime(pitch*100, this.ctx.currentTime);
         oscGain.gain.value = this.state.oscGainValue;
         osc.connect(oscGain);
-        oscGain.connect(this.state.ctx.destination)
+        oscGain.connect(this.ctx.destination)
         this.setState({isOscPlaying: true})
         setTimeout(() => { this.setState({isOscPlaying: false})}, 1000);
         osc.start();
-        oscGain.gain.setValueAtTime(0, this.state.ctx.currentTime + 0.8)
+        oscGain.gain.setValueAtTime(0, this.ctx.currentTime + 0.8)
         setTimeout(() => {
             osc.stop();
         }, 1000)
@@ -270,8 +263,8 @@ export class UserContextStore extends Component{
         .catch(err => {console.log(err)})
     }
     componentWillUnmount(){
-        if(this.state.ctx){
-            this.state.ctx.close();
+        if(this.ctx){
+            this.ctx.close();
         }
         if(this.state.peer){
             this.state.peer.close();
@@ -281,7 +274,6 @@ export class UserContextStore extends Component{
         return(
             <Context.Provider value={{
                 ...this.state,
-                setStartCtxBtn: this.setStartCtxBtn,
                 initUserPlayback: this.initUserPlayback
                 }}>
                 {this.props.children}
