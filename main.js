c4afe77f24d6635a1e1e0da5d6dbf677f7b94d58
Idan Izhaki <izhaki.idan@gmail.com>
2019-02-27 10:08:19 +0200

    Added an IP check on Electron main

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
 const { app, BrowserWindow, Tray, Menu } = require('electron')
 const server = require('./server/server');
 const env = require('dotenv').config();
+
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+// Get localhost ip
+Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+    
+    ifaces[ifname].forEach(function (iface) {
+        if ('IPv4' !== iface.family || iface.internal !== false) {
+            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+            return;
+        }
+        
+        if (alias >= 1) {
+            // this single interface has multiple ipv4 addresses
+            console.log(ifname + ':' + alias, iface.address);
+        } else {
+            // this interface has only one ipv4 adress
+            console.log(ifname, iface.address);
+        }
+        ++alias;
+    });
+});
+
 app.commandLine.appendSwitch('ignore-certificate-errors');
 let appIcon = null;
 function createWindow () {
     // Create the browser window.
     let win = new BrowserWindow({ 
         width: 400, 
-        height: 120,
+        height: 150,
         autoHideMenuBar: true,
         useContentSize: true,
         resizable: false,
@@ -42,16 +67,16 @@ function createWindow () {
             event.preventDefault();
             win.hide();
         }
-    
+        
         return false;
     });
     win.loadFile(__dirname + '/index.html')
     win.focus();
-
+    
     
     
     appIcon.setToolTip('Audio System Server');
-
+    
 }
 
 app.on('ready', createWindow);
